af3f6035fbe01e20126912a72c3f6f58c3cb41c3
atiq100 <atiqur1997rj@gmail.com>
2022-11-24 22:02:06 +0600

    create database and users api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,6 +9,26 @@ const app = express();
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6znnq0v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const usersCollection = client.db('resaleGo').collection('users');
+
+        app.post('/users',async(req,res)=>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
 
 app.get('/',async(req,res)=>{
     res.send('server is running');
