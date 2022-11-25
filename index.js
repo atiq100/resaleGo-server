d9c54160f23b581dd96b505598788b1795ae83c1
atiq100 <atiqur1997rj@gmail.com>
2022-11-26 03:43:57 +0600

    specific category id added

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run(){
     try{
         const usersCollection = client.db('resaleGo').collection('users');
         const bikeCategoriesCollection = client.db('resaleGo').collection('bike-categories');
+        const allBikeCategoriesCollection = client.db('resaleGo').collection('allBike');
 
         app.post('/users',async(req,res)=>{
             const user = req.body;
@@ -28,9 +29,48 @@ async function run(){
         // bike categories
         app.get('/bike-categories',async(req,res)=>{
             const query = {}
+           
             const bikeCategories = await bikeCategoriesCollection.find(query).toArray()
             res.send(bikeCategories);
 
+        })
+
+        app.get('/bike-categories/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const bikeCategories = await bikeCategoriesCollection.findOne(query)
+            res.send(bikeCategories)
+        })
+
+        //bike resale details
+        app.get('/all-bikes',async(req,res)=>{
+            const query={}
+            // if(req.query.category){
+            //     query = {
+            //         category: req.query.category
+            //     }
+            // }
+            // if(req.query.category_id){
+            //     query = {
+            //         category_id: req.query.category_id
+            //     }
+            // }
+            const allBike = await allBikeCategoriesCollection.find(query).toArray()
+            res.send(allBike)
+        })
+       app.get('/all-bikes/:category_id',async(req,res)=>{
+        const category_id = req.params.category_id
+        
+        const query = {category_id: category_id}
+        const allBike = await allBikeCategoriesCollection.find(query).toArray()
+        console.log(allBike);
+            res.send(allBike)
+       })
+
+        app.post('/all-bikes',async(req,res)=>{
+            const query = req.body
+            const allBike = await allBikeCategoriesCollection.insertOne(query)
+            res.send(allBike)
         })
     }
     finally{
