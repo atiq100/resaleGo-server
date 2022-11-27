72e690d558dc173e176261eea5d5827ac03cb7dd
atiq100 <atiqur1997rj@gmail.com>
2022-11-27 21:38:02 +0600

    delete api added

--- index.js
+++ index.js
@@ -46,6 +46,20 @@ async function run(){
             res.send(result);
         })
 
+        app.delete('/bookings/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)}
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
+        })
+
         app.get('/users/admin/:email',async(req,res)=>{
             const email = req.params.email;
             const query = {email}
